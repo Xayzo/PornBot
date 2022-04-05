58b60965f14f94581aecc67c5daf1146664e4660
Xayzo <deonseorang@gmail.com>
2022-04-05 16:42:51 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,30 @@
 from telethon import events, TelegramClient, Button
 from telethon.tl.types import InputMessagesFilterVideo, InputMessagesFilterVoice
-from Dion.porn import telethn as tbot
-from Dion.porn import ubot2 as dbot
+from telethon.sessions import MemorySession
+from pyrogram import Client
 
 import os
 import random
 import logging
 
 
+API_ID = "16813597"
+API_HASH = "6ce811cff9c14820a3352aa5176066b6"
+STRING_SESSION = "1BVtsOJ0Bu0kJpCVwakCO7FNuOb2YWubclYZxpXW_NGvkERlDtkYsFjYltqu5zfc_bQohskBxXS6gVIamVt3tFA3kgkDSVyeo90Ke9kwqquTNX_L66h-4pBKN9nY7QF5R_Kjc3yispcRJc6QIcOZLCkJODJ1kDeuRl762gOHaQFbcqPWA1HwVENIS6cBcA_6QhTqiZb8gZ12YLBya8M2Wzv0fsm1V0vyr5lTJN1-vZjPHy6uwI7_Q6SwIekVI5lfA-kHL0YaLHWV0IjdP27GNs4KBt_IdJNqvyMg0FrdEx-_dLLHe58POljsvxz8Ks7ZuTLGAvQfuQ4XoDLdHGwM-KBx9oRwvKaw="
+
+telethn = TelegramClient(MemorySession(), API_ID, API_HASH)
+
+ubot2 = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+try:
+    ubot2.start()
+except BaseException:
+    print("Userbot Error! Have you added a STRING_SESSION in deploying??")
+    sys.exit(1)
+
+
 logging.basicConfig(level=logging.INFO)
 
 TOKEN = os.environ.get("TOKEN", None)
-STRING_SESSION = os.environ.get("STRING_SESSION", None)
 
 bot = TelegramClient(
         "dion",
